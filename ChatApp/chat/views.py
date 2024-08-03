e4290e9bbeec6049434a8f13ce8aa06fbbc2b47b
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-08-03 22:51:53 +0200

    ChatterBox

--- ChatApp/chat/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-
-
-def chatPage(request, *args, **kwargs):
-    if not request.user.is_authenticated:
-        return redirect("login-user")
-    context = {}
-    return render(request, "chat/chatPage.html", context)
--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ChatConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chat'
+    name = 'core'
