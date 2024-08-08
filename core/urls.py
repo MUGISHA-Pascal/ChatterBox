f6a310b49955855ef11ac23a382d4e84d94920a2
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-08-08 21:21:07 +0200

    signup setup

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("",views.front_page,name="front_page"),
+    path("signup/",views.signup,name="signup")
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .forms import usercreationform
 
 def front_page(request):
     return render(request,"core/frontpage.html")
+
+def signup(request):
+    if request.method=="POST":
+        form = usercreationform(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = usercreationform()
+    return render(request,"core/signup.html")
