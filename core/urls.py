1dc570ef1b4d0d1ad617dc890f84f9f8276963e0
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-08-08 22:56:49 +0200

    login setup

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("",views.front_page,name="front_page"),
     path("signup/",views.signup,name="signup"),
+    path("login/",auth_views.LoginView.as_view(template_name="core/login.html"),name="login"),
     path("logout/",auth_views.LogoutView.as_view(),name="logout")
 ]
